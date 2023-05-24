b1912618d574270d0f69ef8f73ff503fccd76080
jhoisz <jvrda@ufpi.edu.br>
2023-05-24 10:07:38 -0300

    Estilizações finais: ações e tipografia

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -5,7 +5,9 @@ ThemeData myTheme = ThemeData(
   primarySwatch: ThemeColors.primaryColor,
   primaryColor: ThemeColors.primaryColor,
   brightness: Brightness.dark,
+  fontFamily: 'Raleway',
   textTheme: const TextTheme(
+      titleMedium: TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal),
       bodyMedium: TextStyle(fontSize: 16.0),
       bodyLarge: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold)),
 );
